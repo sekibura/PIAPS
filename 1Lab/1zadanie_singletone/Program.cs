b942e0523cf162d2938897690fdcc335ed31e9b3
sekibura <51335422+sekibura@users.noreply.github.com>
2021-02-28 16:53:14 +0300

    Singletone done!

---
+++ 1Lab/1zadanie_singletone/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1zadanie_singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.getInstance();
+            Console.WriteLine(singleton.GetHashCode());
+
+            Singleton singleton2 = Singleton.getInstance();
+            Console.WriteLine(singleton2.GetHashCode());
+        }
+    }
+}
---
+++ 1Lab/1zadanie_singletone/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1zadanie_singletone
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {}
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}
